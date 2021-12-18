9bf3cfdcfeb4c71daede1cde720c115f015f7164
Muhammad Awais <awaisshaikh94@gmail.com>
2021-12-18 23:36:33 +0500

    companies endpoint is ready some fixes was remaining

--- src/modules/companies/companies.module.ts
+++ src/modules/companies/companies.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
+import { Companies, CompaniesSchema } from 'src/schema/companies.schema';
 
 @Module({
   providers: [CompaniesService],
-  controllers: [CompaniesController]
+  controllers: [CompaniesController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Companies.name, schema: CompaniesSchema },
+    ]),
+  ],
 })
 export class CompaniesModule {}
