03d2250422246bde6f4bd8b23d54014c77fb391d
Muhammad Awais <awaisshaikh94@gmail.com>
2021-12-27 16:52:53 +0500

    mongoose fix

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,3 +1,4 @@
 export default {
-    mongoURI: "YOUR_MONGOURI_CONNECTION_STRING"
+    // mongoURI: "YOUR_MONGOURI_CONNECTION_STRING",
+    mongoURI: "mongodb+srv://anisiqbal484:Shopkeryar1@cluster0.b0qqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
